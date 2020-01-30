fa931fde0d201ae540d07c0e3ba4124caa5d6432
cerjakt <cerjakt@dunwoody.edu>
2020-01-30 12:15:16 -0600

    started with the framework for questions and options

--- lab_one/Program.cs
+++ lab_one/Program.cs
@@ -6,14 +6,84 @@ namespace lab_one
     {
         static void Main(string[] args)
         {
-        string[] questions = { "What year did this start?", "When do cla" }
-        int x;
-        //looping structure
-        for(x=0; x < questions.Length; x++){
-                Console.WriteLine(questions[x]);
-                for (y = 0)
-        }
-        
+            string[] answerKey = { "B", "B", "C", "A", "D", "A", "A", "C", "D", "A" }; //Array of answers in order
+            string[] userChoice = new string[10]; //the option the user selected
+            string userSelection; 
+            string[] questions = 
+            {"1. .NET Core is created and maintained by how many developers?",
+             "2. What company develops .NET Core?",
+             "3. ",
+             "4. ",
+             "5. ",
+             "6. ",
+             "7. ",
+             "8. ",
+             "9. ",
+             "10. "
+            };
+            string[,] optionChoices = //sets of 4 options for each question
+            {
+            {
+            "A. ",
+            "B. ",
+            "C. ",
+            "D. "
+            },
+            {
+            "A. ",
+            "B. ",
+            "C. ",
+            "D. "
+            },
+            {
+            "A. ",
+            "B. ",
+            "C. ",
+            "D. "
+            },
+            {
+            "A. ",
+            "B. ",
+            "C. ",
+            "D. "
+            },
+            {
+            "A. ",
+            "B. ",
+            "C. ",
+            "D. "
+            },
+            {
+            "A. ",
+            "B. ",
+            "C. ",
+            "D. "
+            },
+            {
+            "A. ",
+            "B. ",
+            "C. ",
+            "D. "
+            },
+            {
+            "A. ",
+            "B. ",
+            "C. ",
+            "D. "
+            },
+            {
+            "A. ",
+            "B. ",
+            "C. ",
+            "D. "
+            },
+            {
+            "A. ",
+            "B. ",
+            "C. ",
+            "D. "
+            }
+            };
         }
     }
 }
