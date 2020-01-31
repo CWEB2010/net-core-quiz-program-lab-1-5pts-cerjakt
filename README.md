0b45e7f245500f7be4ee254782b19de8277a141c
cerjakt <cerjakt@dunwoody.edu>
2020-01-30 20:11:30 -0600

    Finishing touches

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-# .NET Core Quiz Program (Lab 1 - 5pts.)
-
-You are going to build a quiz application that requires you to generate 10 questions along with 4 option choices for each question.  You will do some research on .NET Core and decide upon 10 questions that will assess the quiz taker's knowledge on .NET Core 
-
 ## Instructions
 
 Listed below is the correct answer key for your quiz.
@@ -11,24 +7,11 @@ Listed below is the correct answer key for your quiz.
  ------	|------	|------	|------	|-------	
  6) A 	| 7) A 	| 8) C 	| 9) D 	| 10) A 	
 ```
-Your program should store these correct answers in an array.  (Store each question’s correct answer in an element of a String array.)  The program will iterate through each question and allow the quiz taker to enter a answer for each question, which should be stored in another array. After the student’s answers have been entered, the program should display a message indicating whether the student passed or failed the exam.  (A student must correctly answer 7 of the 10 questions to pass the quiz.)  It should then display the total number of correctly answered questions, the total number of incorrectly answered questions, and a list showing the question numbers of the incorrectly answered questions.
+Press any key to enter the quiz. If 2 is pressed, the quiz will close. Enter an option of A, B, C, or D to answer a question and proceed onto the next one. At the end of the quiz, it will display your answers followed up with the correct answers. If 1 is pressed, the quiz will restart. If 2 is pressed, the program will end.
 
 
+## Technologies
 
-## Requirements
+C# console code was used to create this program.
 
-- Application should be .NET core console app
-
-- Version control implemented into application with a minimum of 3 commits
-- Make sure master branch is able to successfully compile.
-- ReadMe file is included with project that gives instructions on what the project is and what technologies were used in creating the project.
-
-- Try and utilizing a variety of properties and methods from the Console Class
-- Utilize the placeholder for formatting and outputting string text.
-- Allow the end-user to continuously enter exams until a sentinel value is entered.
-
-## Resources
-[.NET Core Guide](https://docs.microsoft.com/en-us/dotnet/core/)
-
-[Makeareadme.com](https://www.makeareadme.com/)
 
--- lab_one/Program.cs
+++ lab_one/Program.cs
@@ -1,25 +1,27 @@
-﻿using System;
+﻿//J.Cermak
+
+using System;
 
 namespace lab_one
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             string[] answerKey = { "B", "B", "C", "A", "D", "A", "A", "C", "D", "A" }; //Array of answers in order
-            string[] userChoice = new string[] { }; //the option the user selected
-            string userSelection;
-            string[] questions =
+            string[] userChoice; //list of the choices
+            string userSelection; //option user selected
+            string[] questions = //list of questions
             {"1. .NET Core is created and maintained by how many developers?",
-             "2. What company develops .NET Core?",
-             "3. ",
-             "4. ",
-             "5. ",
-             "6. ",
-             "7. ",
-             "8. ",
-             "9. ",
-             "10. "
+             "2. What company primarily develops .NET Core?",
+             "3. What is the current version of .NET Core?",
+             "4. What date was .NET Core released?",
+             "5. What is .NET Core written in?",
+             "6. What is not an operating system for .NET Core?",
+             "7. What is .NET Core released with?",
+             "8. What is the package manager for .NET Core?",
+             "9. When was version 3.1 of .Net Core released?",
+             "10. .NET Core is the successor of what?"
             };
             string[,] optionChoices = //sets of 4 options for each question
             {
@@ -30,104 +32,142 @@ namespace lab_one
             "D. 20,000"
             },
             {
-            "A. ",
-            "B. ",
-            "C. ",
-            "D. "
+            "A. Apple",
+            "B. Microsoft",
+            "C. Google",
+            "D. Samsung"
             },
             {
-            "A. ",
-            "B. ",
-            "C. ",
-            "D. "
+            "A. 2.2",
+            "B. 2.0",
+            "C. 3.1",
+            "D. 3.0"
             },
             {
-            "A. ",
-            "B. ",
-            "C. ",
-            "D. "
+            "A. June 27, 2016",
+            "B. June 26, 2016",
+            "C. June 25, 2016",
+            "D. June 24, 2016"
             },
             {
-            "A. ",
-            "B. ",
-            "C. ",
-            "D. "
+            "A. Swift",
+            "B. Java",
+            "C. Python",
+            "D. C++ & C#"
             },
             {
-            "A. ",
-            "B. ",
-            "C. ",
-            "D. "
+            "A. Chrome OS",
+            "B. Windows",
+            "C. Linux",
+            "D. macOS"
             },
             {
-            "A. ",
-            "B. ",
-            "C. ",
-            "D. "
+            "A. Visual Studio",
+            "B. Komodo IDE",
+            "C. NetBeans",
+            "D. Eclipse"
             },
             {
-            "A. ",
-            "B. ",
-            "C. ",
-            "D. "
+            "A. Snappy",
+            "B. yum",
+            "C. nuGet",
+            "D. Homebrew"
             },
             {
-            "A. ",
-            "B. ",
-            "C. ",
-            "D. "
+            "A. December 6, 2019",
+            "B. December 5, 2019",
+            "C. December 4, 2019",
+            "D. December 3, 2019"
             },
             {
-            "A. ",
-            "B. ",
-            "C. ",
-            "D. "
+            "A. .NET Framework",
+            "B. Cocoa",
+            "C. Cocoa Touch",
+            "D. ActiveX"
             }
             };
 
-            String primingValue; //start the program
+            String primingValue = "D1"; //1 input from user
+            String primingValue2 = "NumPad1"; //numpad 1 input from user
             String EXIT = "NumPad2"; //numpad 2 input from user
             String EXIT2 = "D2"; //2 input from user
-            int x, y;
-            int totalCorrect, totalIncorrect;
+            int x, y; //assigning x and y to integers
+            int totalCorrect, totalIncorrect; //assigning total correct to integers
 
-            totalCorrect = 0;
-            totalIncorrect = 0;
+            Console.WriteLine("Welcome, this is a quiz on .NET Core. This quiz will consist of 3 questions. You must have 7 out of 10 correct to pass the quiz"); //prompt text
+            Console.WriteLine("To get started, please enter any key besides 2."); //prompt text
 
-            Console.WriteLine("Welcome, this is a quiz on .NET Core. This quiz will consist of 3 questions. You must have 2 out of 3 correct to pass the quiz");
-            Console.WriteLine("To get started, please enter any key besides 2.");
+            String userInput = Console.ReadKey().Key.ToString(); //required user input
 
-            String userInput = Console.ReadKey().Key.ToString();
-
-            while (userInput != EXIT || userInput != EXIT2) //initiates while loop if user used a key besides 2 as their input.
+            while (userInput != EXIT && userInput != EXIT2) //initiates while loop if user used a key besides 2 as their input.
             {
-                for (x = 0; x < questions.Length; x++)
-                {
-                    Console.WriteLine(questions[x]);
 
-                    for (y = 0; x < optionChoices.Length; x++)
+                totalCorrect = 0; //setting total to 0 for restarting the quiz
+                totalIncorrect = 0; //setting total to 0 for restarting quiz
+
+                userChoice = new string[answerKey.Length]; //make it so user choice array is the same length as answer key array
+
+                for (x = 0; x < questions.Length; x++) //for loop that ends when it passes 10 questions
+                {
+                    Console.WriteLine("\nPlease enter your selection"); //prompt text
+                    Console.WriteLine(questions[x]); //writes the question
+
+                    for (y = 0; y < optionChoices.GetLength(1); y++) //loop for options choices
                     {
                         Console.WriteLine(optionChoices[x, y]);
                     }
 
-                    Console.WriteLine("Please enter your selection");
+                    userSelection = Console.ReadLine().ToUpper(); //answer input
 
-                    userSelection = Console.ReadLine().ToUpper();
+                    userChoice[x] = userSelection; //adds answer to array of choices
 
-                    if (userSelection == answerKey[x])
+                    if (userSelection == answerKey[x]) //if statement for verifying if your answer was correct or not
                     {
-                        totalCorrect = totalCorrect + 1;
-                        Console.WriteLine("Answer is correct");
+                        totalCorrect++;
+                        Console.WriteLine("You got it right");
                     }
                     else
                     {
                         totalIncorrect++;
-                        Console.WriteLine("Answer is incorrect");
+                        Console.WriteLine("You got it wrong");
                     }
-
                 } //End of while loop
-                Console.WriteLine("Thank you for taking the quiz");
+
+                Console.WriteLine("\nThe quiz has concluded, your results are listed below");
+                Console.WriteLine("\nTotal Correct: " + totalCorrect);
+                Console.WriteLine("\nTotal Incorrect: " + totalIncorrect);
+
+                if (totalCorrect >= 7) //mesasures how many were correct
+                {
+                    Console.WriteLine("\nYou passed the quiz"); //prompt text
+                }
+                else //if you answers less than 2 correct, then this happens
+                {
+                    Console.WriteLine("\nSorry you failed, please retake the quiz"); //prompt text
+                }
+
+                for (int i = 0; i < 10; i++) //iterates through a list telling you your answers and what's correct
+                {
+                    int q = i + 1; //int for question number
+                    Console.WriteLine("\nYour answer for question " + q);
+                    Console.WriteLine(userChoice[i] + ", the correct answer is " + answerKey[i]);
+                }
+
+                bool loop = true; //establishes a true boolean value
+
+                Console.Write("To retake the quiz, please enter 1 or enter 2 to exit the quiz."); //prompt text
+
+                while (loop) //while true
+                {
+                    userInput = Console.ReadKey().Key.ToString(); //input from user
+                    Console.Write("");
+                    if (userInput == primingValue || userInput == primingValue2 || userInput == EXIT || userInput == EXIT2) //if 1 is entered, the quiz will restart
+                    {
+                        loop = false; //breaks loop
+                    }
+                }
             }
+            Console.WriteLine("\nThank you for taking the quiz"); //prompt text
         }
     }
 }
